06082b4d95989248d803f37caa6c0ec7ab13da1f
Nicolás Logosettis <logo.ncls@gmail.com>
2023-04-30 19:41:46 -0300

    canceling request

--- src/apiClients/coinClientAPI.js
+++ src/apiClients/coinClientAPI.js
@@ -2,29 +2,43 @@ import axios from 'axios';
 const ROOT_API_URL = 'https://api.coingecko.com/api/v3';
 let cancelToken;
 
-const cleanRequests = async () => {
+const cleanRequests = () => {
     if (cancelToken) {
-        await cancelToken.cancel()
+        try {
+            cancelToken.cancel();
+            cancelToken = axios.CancelToken.source();
+        } catch (error) {
+            cancelToken = axios.CancelToken.source();
+            //console.log(error)
+        }
     }
-    
-    cancelToken = await axios.CancelToken.source();
+    else {
+        cancelToken = axios.CancelToken.source();
+    }
 }
 
 const GetDefaultCoinList = async () => {
-    await cleanRequests();
-    const response = await axios.get(`${ROOT_API_URL}/search/trending`, cancelToken.token);
+    cleanRequests();
+    const response = await axios.get(`${ROOT_API_URL}/search/trending`, {
+        cancelToken: cancelToken.token
+    });
     return response.data;
 }
 
 const GetCoin = async ({id}) => {
-    await cleanRequests();
-    const response = await axios.get(`${ROOT_API_URL}/coins/${id}`, cancelToken.token);
+    cleanRequests();
+    const response = await axios.get(`${ROOT_API_URL}/coins/${id}`, {
+        cancelToken: cancelToken.token
+    });
     return response.data;
 }
 
 const SearchCoins = async ({searchText}) => {
-    await cleanRequests();
-    const response = await axios.get(`${ROOT_API_URL}/search?query=${searchText}`, cancelToken.token);
+    cleanRequests();
+    cancelToken = axios.CancelToken.source();
+    const response = await axios.get(`${ROOT_API_URL}/search?query=${searchText}`, {
+        cancelToken: cancelToken.token
+    });
     return response.data;
 }
 
