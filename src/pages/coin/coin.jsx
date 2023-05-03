609d1f1a056f42bc5519b0a6e5b9b0ec1e44d37e
Nicolás Logosettis <logo.ncls@gmail.com>
2023-05-03 02:44:15 -0300

    lodash added

--- src/pages/coin/coin.jsx
+++ src/pages/coin/coin.jsx
@@ -3,6 +3,7 @@ import { useSearchParams } from 'react-router-dom';
 import { GetCoin, GetRequestToken, CancelRequestToken } from '../../apiClients/coinClientAPI';
 import { useEffect, useState, useCallback } from "react";
 import Image from 'react-bootstrap/Image';
+import _ from 'lodash';
 import './coin.css';
 
 const Coin = () => {
@@ -34,23 +35,23 @@ const Coin = () => {
                 <div className='col-md-3'>
                     <Image 
                         className='coin-image mx-auto d-block' 
-                        src={coin.image?.large} 
+                        src={_.get(coin, 'image.large')} 
                         alt="Coin"
                         height={150}
                         width={150}
                         roundedCircle={true} />
-                    <h3>{coin.symbol}</h3>
-                    <h5>{coin.name}</h5>
+                    <h3>{_.get(coin, 'symbol')}</h3>
+                    <h5>{_.get(coin, 'name')}</h5>
                     <div className='row'>
-                        <h6><b>Current Price:</b> {coin.market_data?.current_price.usd}</h6>
+                        <h6><b>Current Price:</b> {_.get(coin, 'market_data.current_price.usd')}</h6>
                     </div>
                     <div className='row'>
-                        <h6><b>Supply:</b> {coin.market_data?.total_supply}</h6>
+                        <h6><b>Supply:</b> {_.get(coin, 'market_data.total_supply')}</h6>
                     </div>
                     
                 </div>
                 <div className='col-md-9'>
-                    {coin.description?.en}
+                    {_.get(coin, 'description.en')}
                 </div>
             </div>
         </div>
