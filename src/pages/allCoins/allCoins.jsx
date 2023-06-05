6e5e4bc0e8988e88a9d47fa1538a7ba2c863de8f
Nicolás Logosettis <logo.ncls@gmail.com>
2023-06-05 14:53:47 -0300

    comments updated

--- src/pages/allCoins/allCoins.jsx
+++ src/pages/allCoins/allCoins.jsx
@@ -17,12 +17,13 @@ const AllCoins = () => {
     const [searchError, setSearchError] = useState('');
     const [sort, setSort] = useState(false);
 
-    //this is used just to show or not trending coins h3
-    const [showingTrendingCoins, setShowingTrendingCoins] = useState(true);
 
+    // This is used just to show or not trending coins h3
+    const [showingTrendingCoins, setShowingTrendingCoins] = useState(true);
+    
+
+    // Search for coin list by search text
     const searchCoin = useCallback(async () => {
-        //if (searchText === prevSearch.current) return;
-        
         const { error } = ValidateSearchText({searchText});
 
         setSearchError(error);
@@ -35,6 +36,8 @@ const AllCoins = () => {
     },[searchText])
     
     
+    // Gets default coin list or searched, depending on search text
+    // using useCallback hook to prevents rewriting on rendering
     const GetCoins = useCallback(async () => {
         const coinList = searchText ? await searchCoin() : await GetDefaultCoinList({requestToken});
         setShowingTrendingCoins(searchText === '');
@@ -44,8 +47,49 @@ const AllCoins = () => {
         setCoins(coinsMapped);
     }, [searchText, searchCoin]);
 
+
+    // Creates a debounce function for GetCoins
     const GetCoinsDebounced =  _.debounce(GetCoins, 500);
 
+
+    // Updates the sort state
+    const handleSort = () => {
+        setSort(!sort);
+    }
+
+    
+    // It sorts the current coin list
+    // using useMemo hook to prevents rewriting this function when rendering
+    useMemo(() => {
+        if (sort) {
+            setCoins(coins.sort((a, b) => a.name.localeCompare(b.name)))
+        }
+    }, [sort, coins])
+
+
+    // It updates the provider with the current search
+    // to later be used when the user come back to this page
+    const handleSearchText = (text) => {
+        dispatch({
+            type: 'UPDATE_SEARCH_TEXT',
+            value: text
+        })
+    }
+
+
+    // Is triggered when the search texts is modified
+    // uses debounce to wait until the user stop typing 
+    useEffect(() => {
+        console.log('searchText modified')
+        GetCoinsDebounced();
+        return () => GetCoinsDebounced.cancel();
+        //not included GetCoinsDebounced to avoid infinite loop
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [searchText]);
+
+
+    // Runs just once the first time the component is rendered
+    // gets a token and gets the default coins
     useEffect(() => {
         requestToken = GetRequestToken();
         GetCoinsDebounced();
@@ -54,31 +98,6 @@ const AllCoins = () => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     },[]);
-
-    const handleSort = () => {
-        setSort(!sort);
-    }
-
-    useMemo(() => {
-        if (sort) {
-            setCoins(coins.sort((a, b) => a.name.localeCompare(b.name)))
-        }
-    }, [sort, coins])
-
-    const handleSearchText = (text) => {
-        dispatch({
-            type: 'UPDATE_SEARCH_TEXT',
-            value: text
-        })
-    }
-
-         useEffect(() => {
-            console.log('searchText modified')
-         GetCoinsDebounced();
-         return () => GetCoinsDebounced.cancel();
-         //not included GetCoinsDebounced to avoid infinite loop
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [searchText])
 
     return(
         <>
